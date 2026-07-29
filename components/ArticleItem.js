85a9369be2ea8ee12ed2646ae59974fea189fa03
anugupta01 <114245303+anugupta01@users.noreply.github.com>
2026-07-29 22:21:35 +0530

    updated

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,11 +3,9 @@ import articleStyles from '../styles/Article.module.css'
 
 const ArticleItem = ({ article }) => {
   return (
-    <Link href={`/article/${article.id}`}>
-      <a className={articleStyles.card}>
-        <h3>{article.title} &rarr;</h3>
-        <p>{article.excerpt}</p>
-      </a>
+    <Link href={`/article/${article.id}`} className={articleStyles.card}>
+      <h3>{article.title} &rarr;</h3>
+      <p>{article.excerpt}</p>
     </Link>
   )
 }
